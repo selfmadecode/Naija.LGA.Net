ace73b4fa1f731e6a650a2859c098f32d64de4e3
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-11-27 21:43:47 +0100

    add state and lga model

---
+++ src/Naija.Grid.Net/Model/LocalGovernment.cs
@@ -0,0 +1,9 @@
+﻿namespace Naija.Grid.Net.Model
+{
+    internal class LocalGovernment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Population { get; set; }
+    }
+}
---
+++ src/Naija.Grid.Net/Model/State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Naija.Grid.Net.Model
+{
+    internal class State
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public IEnumerable<LocalGovernment> LocalGovernments { get; set; } = new List<LocalGovernment>();
+    }
+}

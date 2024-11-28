6328b1d2ddec745d510d7d0f6ce8b250451952f4
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-11-28 09:27:03 +0100

    add Akwa Ibom state and Lga

--- src/Naija.Grid.Net/NaijaGridService.cs
+++ src/Naija.Grid.Net/NaijaGridService.cs
@@ -67,6 +67,46 @@ namespace Naija.Grid.Net
                     new LGA { Id = 20, Name = "Yola North" },
                     new LGA { Id = 21, Name = "Yola South" }
                 }
+            },
+            new State
+            {
+                Id = 3,
+                Name = "Akwa Ibom",
+                Capital = "Uyo",
+                LGAs = new List<LGA>
+                {
+                    new LGA { Id = 1, Name = "Abak"},
+                    new LGA { Id = 2, Name = "Eastern Obolo"},
+                    new LGA { Id = 3, Name = "Eket"},
+                    new LGA { Id = 4, Name = "Esit Eket"},
+                    new LGA { Id = 5, Name = "Essien Udim"},
+                    new LGA { Id = 6, Name = "Etim Ekpo"},
+                    new LGA { Id = 7, Name = "Etinan"},
+                    new LGA { Id = 8, Name = "Ibeno"},
+                    new LGA { Id = 9, Name = "Ibesikpo Asutan"},
+                    new LGA { Id = 10, Name = "Ibiono-Ibom"},
+                    new LGA { Id = 11, Name = "Ikot Abasi"},
+                    new LGA { Id = 12, Name = "Ika"},
+                    new LGA { Id = 13, Name = "Ikono"},
+                    new LGA { Id = 14, Name = "Ikot Ekpene"},
+                    new LGA { Id = 15, Name = "Ini"},
+                    new LGA { Id = 16, Name = "Mkpat-Enin"},
+                    new LGA { Id = 17, Name = "Itu"},
+                    new LGA { Id = 18, Name = "Mbo"},
+                    new LGA { Id = 19, Name = "Nsit-Atai"},
+                    new LGA { Id = 20, Name = "Nsit-Ibom"},
+                    new LGA { Id = 21, Name = "Nsit-Ubium"},
+                    new LGA { Id = 22, Name = "Obot Akara"},
+                    new LGA { Id = 23, Name = "Okobo"},
+                    new LGA { Id = 24, Name = "Onna"},
+                    new LGA { Id = 25, Name = "Oron"},
+                    new LGA { Id = 26, Name = "Udung-Uko"},
+                    new LGA { Id = 27, Name = "Ukanafun"},
+                    new LGA { Id = 28, Name = "Oruk Anam"},
+                    new LGA { Id = 29, Name = "Uruan"},
+                    new LGA { Id = 30, Name = "Urue-Offong/Oruko"},
+                    new LGA { Id = 31, Name = "Uyo"},
+                }
             }
         };
     }

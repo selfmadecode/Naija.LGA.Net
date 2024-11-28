5d836c56f85311d9cac0846fff71e1cc68ad023f
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-11-28 09:24:22 +0100

    added adamawa state and lga

--- src/Naija.Grid.Net/NaijaGridService.cs
+++ src/Naija.Grid.Net/NaijaGridService.cs
@@ -17,25 +17,55 @@ namespace Naija.Grid.Net
                 Id = 1,
                 Name = "Abia",
                 Capital = "Umuahia",
-                LocalGovernments = new List<LocalGovernment>
+                LGAs = new List<LGA>
                 {
-                    new LocalGovernment { Id = 1, Name = "Aba North", Population = 0 },
-                    new LocalGovernment { Id = 2, Name = "Aba South", Population = 0 },
-                    new LocalGovernment { Id = 3, Name = "Arochukwu", Population = 0 },
-                    new LocalGovernment { Id = 4, Name = "Bende", Population = 0 },
-                    new LocalGovernment { Id = 5, Name = "Ikwuano", Population = 0 },
-                    new LocalGovernment { Id = 6, Name = "Isiala Ngwa North", Population = 0 },
-                    new LocalGovernment { Id = 7, Name = "Isiala Ngwa South", Population = 0 },
-                    new LocalGovernment { Id = 8, Name = "Isiukwuato", Population = 0 },
-                    new LocalGovernment { Id = 9, Name = "Obingwa", Population = 0 },
-                    new LocalGovernment { Id = 10, Name = "Ohafia", Population = 0 },
-                    new LocalGovernment { Id = 11, Name = "Osisioma Ngwa", Population = 0 },
-                    new LocalGovernment { Id = 12, Name = "Ugwunagbo", Population = 0 },
-                    new LocalGovernment { Id = 13, Name = "Ukwa East", Population = 0 },
-                    new LocalGovernment { Id = 14, Name = "Ukwa West", Population = 0 },
-                    new LocalGovernment { Id = 15, Name = "Umuahia North", Population = 0 },
-                    new LocalGovernment { Id = 16, Name = "Umuahia South", Population = 0 },
-
+                    new LGA { Id = 1, Name = "Aba North"},
+                    new LGA { Id = 2, Name = "Aba South"},
+                    new LGA { Id = 3, Name = "Arochukwu"},
+                    new LGA { Id = 4, Name = "Bende"},
+                    new LGA { Id = 5, Name = "Ikwuano"},
+                    new LGA { Id = 6, Name = "Isiala Ngwa North"},
+                    new LGA { Id = 7, Name = "Isiala Ngwa South"},
+                    new LGA { Id = 8, Name = "Isiukwuato"},
+                    new LGA { Id = 9, Name = "Obingwa"},
+                    new LGA { Id = 10, Name = "Ohafia"},
+                    new LGA { Id = 11, Name = "Osisioma Ngwa"},
+                    new LGA { Id = 12, Name = "Ugwunagbo"},
+                    new LGA { Id = 13, Name = "Ukwa East"},
+                    new LGA { Id = 14, Name = "Ukwa West"},
+                    new LGA { Id = 15, Name = "Umuahia North"},
+                    new LGA { Id = 16, Name = "Umuahia South"},
+                    new LGA { Id = 17, Name = "Umu Nneochi"}
+                }
+            },
+            new State
+            {
+                Id = 2,
+                Name = "Adamawa",
+                Capital = "Yola",
+                LGAs = new List<LGA>
+                {
+                    new LGA { Id = 1, Name = "Demsa" },
+                    new LGA { Id = 2, Name = "Fufure" },
+                    new LGA { Id = 3, Name = "Ganye" },
+                    new LGA { Id = 4, Name = "Gayuk" },
+                    new LGA { Id = 5, Name = "Gombi" },
+                    new LGA { Id = 6, Name = "Grie" },
+                    new LGA { Id = 7, Name = "Hong" },
+                    new LGA { Id = 8, Name = "Jada" },
+                    new LGA { Id = 9, Name = "Larmurde" },
+                    new LGA { Id = 10, Name = "Madagali" },
+                    new LGA { Id = 11, Name = "Maiha" },
+                    new LGA { Id = 12, Name = "Mayo Belwa" },
+                    new LGA { Id = 13, Name = "Michika" },
+                    new LGA { Id = 14, Name = "Mubi North" },
+                    new LGA { Id = 15, Name = "Mubi South" },
+                    new LGA { Id = 16, Name = "Numan" },
+                    new LGA { Id = 17, Name = "Shelleng" },
+                    new LGA { Id = 18, Name = "Song" },
+                    new LGA { Id = 19, Name = "Toungo" },
+                    new LGA { Id = 20, Name = "Yola North" },
+                    new LGA { Id = 21, Name = "Yola South" }
                 }
             }
         };

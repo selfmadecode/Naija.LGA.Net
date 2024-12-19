0eb6ef84f466f58f0d1a2b2e2dc417e200d5789a
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-12-19 22:00:37 +0100

    add sonsole app

---
+++ Naija.Grid.App/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Naija.Grid.Net;
+
+var states = NaijaGridService.GetStates();
+
+Console.WriteLine(states.Count());
+Console.WriteLine("Hello, World!");

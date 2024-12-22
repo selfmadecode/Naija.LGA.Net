ed483f8a12ad1936155ac1bb45122319ac909675
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-12-22 20:05:33 +0100

    test package

--- Naija.Grid.App/Program.cs
+++ Naija.Grid.App/Program.cs
@@ -4,4 +4,10 @@ using Naija.Grid.Net;
 var states = NaijaGridService.GetStates();
 
 Console.WriteLine(states.Count());
+
+var state = NaijaGridService.GetState(36);
+var imo = NaijaGridService.GetState("Imo");
+
+Console.WriteLine(state.Name);
+Console.WriteLine(imo.Name);
 Console.WriteLine("Hello, World!");

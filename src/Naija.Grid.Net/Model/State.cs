1b094da395b8ac48615e6c49b4c10367e2c3d4bb
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-12-28 09:52:04 +0100

    change LGAs to Lgas

--- src/Naija.Grid.Net/Model/State.cs
+++ src/Naija.Grid.Net/Model/State.cs
@@ -9,6 +9,6 @@ namespace Naija
         public int Id { get; set; }
         public string Name { get; set; }
         public string Capital { get; set; }
-        public IEnumerable<Lga> LGAs { get; set; } = new List<Lga>();
+        public IEnumerable<Lga> Lgas { get; set; } = new List<Lga>();
     }
 }
